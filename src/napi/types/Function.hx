b8870374ed87867e59b7ccebd87a2ab9050f43d1
Kevin Leung <kevinresol@gmail.com>
2017-02-25 21:53:31 +0800

    p for parameter, t for type

--- src/napi/types/Function.hx
+++ src/napi/types/Function.hx
@@ -16,16 +16,16 @@ class Function<Rest> {}
 @:native('System.Action') extern class Action__1<T1> {}
 @:dce abstract Action_1<T1>(Action__1<T1>) {
 	@:from public static inline function ofFunc<T1>(f:T1->Void)
-		return untyped __cs__('(t1) => {0}', f(t1));
-	public inline function call(t1:T1)
-		untyped __cs__('{0}({1})', this, t1);
+		return untyped __cs__('(p1) => {0}', f(p1));
+	public inline function call(p1:T1)
+		untyped __cs__('{0}({1})', this, p1);
 }
 @:native('System.Action') extern class Action__2<T1, T2> {}
 @:dce abstract Action_2<T1, T2>(Action__2<T1, T2>) {
 	@:from public static inline function ofFunc<T1, T2>(f:T1->T2->Void)
-		return untyped __cs__('(t1, t2) => {0}', f(t1, t2));
-	public inline function call(t1:T1, t2:T2)
-		untyped __cs__('{0}({1}, {2})', this, t1, t2);
+		return untyped __cs__('(p1, p2) => {0}', f(p1, p2));
+	public inline function call(p1:T1, p2:T2)
+		untyped __cs__('{0}({1}, {2})', this, p1, p2);
 }
 
 @:native('System.Func') extern class Func__0<R> {}
@@ -39,15 +39,15 @@ class Function<Rest> {}
 @:native('System.Func') extern class Func__1<T1, R> {}
 @:dce abstract Func_1<T1, R>(Func__1<T1, R>) {
 	@:from public static inline function ofFunc<T1, R>(f:T1->R)
-		return untyped __cs__('(t1) => {0}', f(t1));
-	public inline function call(t1:T1):R
-		return untyped __cs__('{0}({1})', this, t1);
+		return untyped __cs__('(p1) => {0}', f(p1));
+	public inline function call(p1:T1):R
+		return untyped __cs__('{0}({1})', this, p1);
 }
 @:native('System.Func') extern class Func__2<T1, T2, R> {}
 @:dce abstract Func_2<T1, T2, R>(Func__2<T1, T2, R>) {
 	@:from public static inline function ofFunc<T1, T2, R>(f:T1->T2->R)
-		return untyped __cs__('(t1, t2) => {0}', f(t1, t2));
-	public inline function call(t1:T1, t2:T2):R
-		return untyped __cs__('{0}({1}, {2})', this, t1, t2);
+		return untyped __cs__('(p1, p2) => {0}', f(p1, p2));
+	public inline function call(p1:T1, p2:T2):R
+		return untyped __cs__('{0}({1}, {2})', this, p1, p2);
 }
 #end
